c9410851f85f49c466aa35c8d93d9fe04c390321
xiaoshidefeng <1330661071@qq.com>
2016-12-22 19:47:40 +0800

    修复Android6.0 回复框的一些问题

--- SlideMeueTest/app/src/main/java/com/example/cw/slidemeuetest/MainActivity.java
+++ SlideMeueTest/app/src/main/java/com/example/cw/slidemeuetest/MainActivity.java
@@ -152,9 +152,13 @@ public class MainActivity extends AppCompatActivity
         toolbar = (Toolbar) findViewById(R.id.id_toolbar);
 //        mTouchSlop = ViewConfiguration.get(this).getScaledTouchSlop();
 
-        //透明状态栏实现
-        getWindow().setStatusBarColor(Color.TRANSPARENT);
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        if(Build.VERSION.SDK_INT > 21){
+            //透明状态栏实现
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+
+        }
+
 
 //        webView=(WebView)findViewById(R.id.id_webView);
 //        webView.getSettings().setJavaScriptEnabled(true);
