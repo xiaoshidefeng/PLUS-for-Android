f33da02b1b3fe44429f9dbc3c5223dbcc8098961
xiaoshidefeng <1330661071@qq.com>
2017-11-23 18:19:32 +0800

    重构 发帖完成

--- SlideMeueTest/app/src/main/java/com/example/cw/slidemeuetest/MainActivityFragment/Fragmenttwo/NewPostActivity.java
+++ SlideMeueTest/app/src/main/java/com/example/cw/slidemeuetest/MainActivityFragment/Fragmenttwo/NewPostActivity.java
@@ -1,11 +1,10 @@
 package com.example.cw.slidemeuetest.MainActivityFragment.Fragmenttwo;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -15,17 +14,23 @@ import android.widget.Toast;
 
 import com.example.cw.slidemeuetest.MainActivity;
 import com.example.cw.slidemeuetest.R;
-import com.example.cw.slidemeuetest.LoginRegist.Register_main;
+import com.example.cw.slidemeuetest.util.DisscussConst;
+import com.example.cw.slidemeuetest.util.IsNull;
+import com.example.cw.slidemeuetest.util.TokenUtil;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
+import java.lang.ref.WeakReference;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class NewPostActivity extends AppCompatActivity {
 
@@ -38,33 +43,26 @@ public class NewPostActivity extends AppCompatActivity {
     //进度条
     private ProgressBar progressBar;
 
-
     //内容输入
     private EditText etContent;
 
     private ImageView imSend;
-
-    //更新token api
-    private static String tokenValidTestUrl = "http://lsuplus.top/api/refresh/?token=";
-
-    //发贴
-    private static String NewPostUrl = "http://lsuplus.top/api/discuss/store?token=";
-
-    //token
-    private String token;
-    //用户id
-    private String userid;
 
     //标题
     private String title;
     //内容
     private String content;
 
+    private Handler myHandler;
+
+    private OkHttpClient client = new OkHttpClient();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
 
+        myHandler = new MyHandler(this);
         initview();
 
         //隐藏进度条
@@ -92,6 +90,14 @@ public class NewPostActivity extends AppCompatActivity {
         etContent = (EditText)findViewById(R.id.id_etpostcontent);
         imSend = (ImageView)findViewById(R.id.id_IMNewpost);
         progressBar = (ProgressBar)findViewById(R.id.id_Pbnewpost);
+
+        try {
+            TokenUtil.reFreshToken(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     //输入格式提示
@@ -111,9 +117,6 @@ public class NewPostActivity extends AppCompatActivity {
         title = etTitle.getText().toString();
         content = etContent.getText().toString();
 
-//        content.replace("\n","\n\n");
-        Log.e("status",content);
-
         // 标题判断
         if(title.equals("")||title==null){
             etTitle.setError(getString(R.string.error_null_posttitle));
@@ -127,184 +130,84 @@ public class NewPostActivity extends AppCompatActivity {
             cancel = true;
         }
 
-
         if (cancel) {
             focusView.requestFocus();
         } else {
+            if(IsNull.isNullField(TokenUtil.getToken(this))){
+                Toast.makeText(NewPostActivity.this,"请先登录",Toast.LENGTH_SHORT).show();
+                return;
+            }
             //进度条开始转动
             progressBar.setVisibility(View.VISIBLE);
 
-            //访问网络
-            getuserinfo();
+            //发帖
+            creatPost();
         }
     }
 
-    private void getuserinfo() {
-        SharedPreferences sharedPreferences = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
-        token = sharedPreferences.getString("token","");
-        int i =  sharedPreferences.getInt("id",0);
-        userid = String.valueOf(i);
 
-        if(token.equals("")){
-            Toast.makeText(NewPostActivity.this,"请先登录",Toast.LENGTH_SHORT).show();
-            progressBar.setVisibility(View.GONE);
+    static class MyHandler extends Handler {
+        WeakReference<NewPostActivity> mActivityReference;
 
-            return;
+        MyHandler(NewPostActivity activity) {
+            mActivityReference = new WeakReference<NewPostActivity>(activity);
         }
 
-        RefreshToken();
-    }
-
-    //更新token
-    private void RefreshToken(){
-
-        //测试token是否过期
-        //开启子线程访问网络 测试token模块
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-                HttpURLConnection connection = null;
+        @Override
+        public void handleMessage(Message msg) {
+            NewPostActivity activity = mActivityReference.get();
+            if (msg.what == 1) {
                 try {
-
-                    URL url = new URL(tokenValidTestUrl+token);
-
-                    connection = (HttpURLConnection)url.openConnection();
-                    connection.setRequestMethod("GET");
-                    connection.connect();
-
-                    //连接超时设置
-                    connection.setConnectTimeout(8000);
-                    connection.setReadTimeout(8000);
-
-                    //获取输入流
-                    InputStream in = connection.getInputStream();
-
-                    //对获取的流进行读取
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(in,"utf-8"));
-                    final StringBuilder response = new StringBuilder();
-                    String line=null;
-                    while ((line=reader.readLine())!=null){
-                        response.append(line);
-                    }
-
-                    token = connection.getHeaderField("Authorization");
-                    token = token.substring(7,token.length());
-                    savaToken();
-                    sendHttpURLConnectionNewPost();
-
-                }   catch (Exception e) {
-                    try {
-                        int status_code = connection.getResponseCode();
-                        if (status_code==400){
-                            String error = connection.getResponseMessage();
-                            if(error == "token_invalid"){
-                                //登录时间到达两周 需要重新登录
-
-                                Toast.makeText(NewPostActivity.this,"长时间未登录 请重新登录！",Toast.LENGTH_SHORT).show();
-
-                                //跳转到登录界面
-                                Intent intent = new Intent(NewPostActivity.this, Register_main.class);
-                                startActivity(intent);
-
-
-                            }else {
-
-                                Toast.makeText(NewPostActivity.this,"未知错误！",Toast.LENGTH_SHORT).show();
-                            }
-
-                        }
-                    } catch (IOException e1) {
-                        e1.printStackTrace();
-                    }
-                    Log.e("errss", e.getMessage());
+                    formatPost(activity, new JSONObject(msg.obj.toString()));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } finally {
+                    //隐藏进度条
+                    activity.progressBar.setVisibility(View.GONE);
                 }
             }
-        }).start();
+        }
 
-    }
-
-    private void savaToken() {
-        //保存token
-        SharedPreferences sharedPreferences = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("token",token);
-        editor.commit();
-    }
-
-    private void sendHttpURLConnectionNewPost() {
-        //开启子线程访问网络 回复帖子模块
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-                HttpURLConnection connection = null;
-                try {
-//                    String newpostcontent =token+ "&title=" + title+"&body="+content+
-//                            "&user_id="+userid;
-                    URL url = new URL(NewPostUrl+token);
-                    Log.e("status",url.toString());
-
-                    connection = (HttpURLConnection)url.openConnection();
-                    connection.setRequestMethod("POST");
-//                    connection.connect();
-                    //连接超时设置
-                    connection.setConnectTimeout(8000);
-                    connection.setReadTimeout(8000);
-                    //设置运行输入,输出:
-                    connection.setDoOutput(true);
-                    connection.setDoInput(true);
-                    //Post方式不能缓存,需手动设置为false
-                    connection.setUseCaches(false);
-                    //2设置http请求数据的类型为表单类型
-
-//                    connection.setRequestProperty("Content-type","application/x-www-form-urlencoded");
-
-                    String data = "title=" + URLEncoder.encode(title,"utf-8") + "&body=" +
-                            URLEncoder.encode(content,"utf-8")+"&user_id=" +
-                            URLEncoder.encode(userid,"utf-8");
-                    OutputStream out = connection.getOutputStream();
-                    out.write(data.getBytes());
-                    out.flush();
-
-                    if (connection.getResponseCode() == 200) {
-                        // 获取响应的输入流对象
-                        InputStream is = connection.getInputStream();
-                        // 创建字节输出流对象
-                        ByteArrayOutputStream message = new ByteArrayOutputStream();
-                        // 定义读取的长度
-                        int len = 0;
-                        // 定义缓冲区
-                        byte buffer[] = new byte[1024];
-                        // 按照缓冲区的大小，循环读取
-                        while ((len = is.read(buffer)) != -1) {
-                            // 根据读取的长度写入到os对象中
-                            message.write(buffer, 0, len);
-                        }
-                        // 释放资源
-                        is.close();
-
-                    }
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                //隐藏进度条
-                                progressBar.setVisibility(View.GONE);
-                                Toast.makeText(NewPostActivity.this,"发帖成功",Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(NewPostActivity.this, MainActivity.class);
-                                startActivity(intent);
-                            }
-                        });
-
-
-                }   catch (Exception e) {
-
-                    Log.e("error", e.getMessage());
-
-                }
+        private void formatPost(NewPostActivity activity, JSONObject jsonObject) throws JSONException {
+            if (jsonObject.has("data")) {
+                Intent intent = new Intent(activity, MainActivity.class);
+                activity.startActivity(intent);
+            } else if (jsonObject.has("error")) {
+                Toast.makeText(activity, jsonObject.getString("error"), Toast.LENGTH_SHORT).show();
             }
-        }).start();
+        }
     }
 
+    /**
+     * 发新帖子
+     * 目前全部都是默认日常帖子 （暂时没空改UI）
+     * @param
+     */
+    private void creatPost() {
+        RequestBody body = new FormBody.Builder()
+                .add("body", content)//添加键值对
+                .add("title", title)
+                .add("categories", "daily")
+                .build();
+        Request request = new Request.Builder()
+                .url(DisscussConst.NEW_POST)
+                .addHeader("Authorization", "Bearer " + TokenUtil.getToken(getApplicationContext()))
+                .post(body)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Message msg = new Message();
+                msg.what = 1;
+                msg.obj = response.body().string();
+                myHandler.sendMessage(msg);
+            }
+        });
+    }
 
 }
